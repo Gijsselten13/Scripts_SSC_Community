f481933ff90dbc9340b28dbeaa16cc21681c5c7f
Gijsselten13 <117294807+Gijsselten13@users.noreply.github.com>
2026-04-25 13:47:03 +0200

    Create Ternary_KOs_av_med_original.R

---
+++ Shiny_app_files/Ternary_KOs_av_med_original.R
@@ -0,0 +1,80 @@
+library("dplyr") #Version 1.1.2
+
+working_directory <- ""
+results_sa.dir <- paste(working_directory, "Shiny_app/", sep = "")
+
+###Generation of ternary_KOs_av_med_all.txt - FC of isolates for each KO (split over 4 SynComs) - Original dataset =====
+KO_table = read.table(paste(working_directory, "KO_genome/KO_SSC.tsv", sep = ""), header=TRUE,sep="\t", row.names = 1)
+colnames(KO_table) <- gsub("X", "", colnames(KO_table))
+
+tax_df = read.table(paste(working_directory,"SSC_taxonomy_GTDB.tsv",sep = ""), header=T,sep="\t",quote="\"", fill = FALSE)
+rownames(tax_df) <- tax_df$isolate
+tax_df_2 <- tax_df %>% dplyr::select (-isolate)
+
+input_table_3 <- row.names(KO_table)
+
+SynComs <- c("AtSC","LjSC", "HvSC", "SSC")
+hop_2 <- data.frame()
+
+for (KO in input_table_3){
+  for (syncom in SynComs){
+    syncom_table <- read.table(paste(working_directory, "Isolate_tables/Original/", syncom,"_norm.tsv", sep= ""), sep = "\t", header =T, row.names =1)
+    syncom_table_2 <- syncom_table[,grep("ES", colnames(syncom_table))]
+    syncom_table_3 <- syncom_table_2[,grep(paste(syncom), colnames(syncom_table_2))]
+    syncom_table_4 <- syncom_table_3[,!grepl("HL", colnames(syncom_table_3))]
+    
+    KO_table_sub <- KO_table[row.names(KO_table) == paste(KO),]
+    
+    if (syncom == "SSC"){
+      KO_table_sub_2 <- KO_table_sub
+    } else {
+      KO_table_sub_2 <- KO_table_sub[,colnames(KO_table_sub) %in% row.names(tax_df_2)[tax_df_2$SynCom == paste(syncom)]]
+    }
+    
+    KO_table_sub_yes <- names(KO_table_sub_2)[KO_table_sub_2 > 0]
+    KO_table_sub_no <- names(KO_table_sub_2)[KO_table_sub_2 == 0]
+    
+    syncom_table_5 <- t(t(syncom_table_4)/rowSums(t(syncom_table_4)))
+    
+    syncom_table_At <- syncom_table_5[,grep("At_", colnames(syncom_table_5))]
+    syncom_table_Hv <- syncom_table_5[,grep("Hv_", colnames(syncom_table_5))]
+    syncom_table_Lj <- syncom_table_5[,grep("Lj_", colnames(syncom_table_5))]
+    
+    #Averages
+    syncom_table_At_2 <- rowSums(syncom_table_At)/length(colnames(syncom_table_At))
+    syncom_table_Hv_2 <- rowSums(syncom_table_Hv)/length(colnames(syncom_table_Hv))
+    syncom_table_Lj_2 <- rowSums(syncom_table_Lj)/length(colnames(syncom_table_Lj))
+    
+    At_RA <- sum(syncom_table_At_2[names(syncom_table_At_2) %in% KO_table_sub_yes])
+    Hv_RA <- sum(syncom_table_Hv_2[names(syncom_table_Hv_2) %in% KO_table_sub_yes])
+    Lj_RA <- sum(syncom_table_Lj_2[names(syncom_table_Lj_2) %in% KO_table_sub_yes])
+    
+    syncom_table_inp <- syncom_table[,grep("Input", colnames(syncom_table))]
+    syncom_table_inp_2 <- t(t(syncom_table_inp)/rowSums(t(syncom_table_inp)))
+    
+    syncom_table_inp_3  <- rowSums(syncom_table_inp_2)/length(colnames(syncom_table_inp_2))
+    
+    Input_RA <- sum(syncom_table_inp_3[names(syncom_table_inp_3) %in% KO_table_sub_yes])
+    
+    if(Input_RA == 0){
+      At_val <- At_RA
+      Hv_val <- Hv_RA
+      Lj_val <- Lj_RA
+    } else {
+      At_val <- At_RA/Input_RA
+      Hv_val <- Hv_RA/Input_RA
+      Lj_val <- Lj_RA/Input_RA
+    }
+    
+    len_val <- length(KO_table_sub_yes)/length(KO_table_sub_2)
+    
+    hop <- t(data.frame(c(paste(KO), At_val, Hv_val, Lj_val, len_val, paste(syncom), length(colnames(syncom_table_At)), length(colnames(syncom_table_Hv)),length(colnames(syncom_table_Lj)))))
+    
+    hop_2 <- rbind(hop_2, hop)
+  }
+}
+
+row.names(hop_2) <- NULL
+colnames(hop_2) <- c("KO", "Arabidopsis", "Barley", "Lotus", "Proportion_of_strains", "SynCom", "No_of_samples_At", "No_of_samples_Hv", "No_of_samples_Lj")
+
+write.table(hop_2, paste(results_sa.dir, "ternary_KOs_av_med_all.txt", sep = ""), col.names=T, row.names =T, quote =F, sep = "\t")
